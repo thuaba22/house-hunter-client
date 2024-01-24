c5ee0fd657347b23269dedc707a55f65bad6eb69
Thuaba Suma Binth Islam Mim <mim.islam.tabassuma@gmail.com>
2024-01-24 11:52:05 +0600

    navbar created

---
+++ src/components/Navbar/Logo.jsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+  return (
+    <div className="flex items-center">
+      <img
+        className=""
+        src="https://i.postimg.cc/rp9MdpqD/footer-logo.png"
+        alt="logo"
+      />
+    </div>
+  );
+};
+
+export default Logo;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,9 @@
+import Navbar from "../../components/Navbar/Navbar";
+
 const Home = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <h3 className="text-3xl">This is home</h3>
     </div>
   );
