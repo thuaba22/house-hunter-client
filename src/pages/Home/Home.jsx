5a8074efe96784a6e8855707151844cc6e3c4eca
Thuaba Suma Binth Islam Mim <mim.islam.tabassuma@gmail.com>
2024-01-24 12:10:00 +0600

    banner complete

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
+import Banner from "../../components/Banner/Banner";
 import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/footer/footer";
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <Banner></Banner>
       <h3 className="text-3xl">This is home</h3>
+      <Footer></Footer>
     </div>
   );
 };
