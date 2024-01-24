824a2a060811a8a4c65039b622e40502c3b02c81
Thuaba Suma Binth Islam Mim <mim.islam.tabassuma@gmail.com>
2024-01-24 10:56:48 +0600

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h2>This Navbar</h2>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h3 className="text-3xl">This is home</h3>
+    </div>
+  );
+};
+
+export default Home;
